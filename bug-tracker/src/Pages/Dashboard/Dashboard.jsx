49315ee66254f8604061a9baf27f3f0d9de9f60c
Keanu Barnard <keanu.barnardd@gmail.com>
2022-06-01 19:33:16 +1000

    Added Select input styling

--- bug-tracker/src/Pages/Dashboard/Dashboard.jsx
+++ bug-tracker/src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import StatsCard from "../../Components/StatsCard/StatsCard";
-import './Dashboard.scss';
+import "./Dashboard.scss";
 
 export default function Dashboard() {
   return (
@@ -12,6 +12,16 @@ export default function Dashboard() {
           <StatsCard priority="medium" title="Medium" value="1" />
           <StatsCard priority="high" title="High" value="2" />
           <StatsCard priority="resolved" title="Resolved" value="7" />
+        </div>
+        <h1 className="head-2" >All your current bugs</h1>
+        <select name="" id="">
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+          <option value="resolved">Resolved</option>
+        </select>
+        <div className="bug-grid">
+
         </div>
       </div>
     </div>
